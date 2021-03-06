898a89112326356df6ccf2f2b158bfaed6214be4
Collin Pfeifer <collinpfeifer@Icloud.com>
2021-03-06 11:23:39 -0500

    attempt to fix component height glitching on mobile
    attempt to fix component glitching mobile

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -11,7 +11,7 @@ const Layout = ({children, page}) => {
     <div className={styles.container}>
       <Helmet>
         <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, height=device-height initial-scale=1" />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
