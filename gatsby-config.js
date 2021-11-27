c027bef39e9a75dda7306a3fea58040b8b693712
Collin Pfeifer <collinpfeifer@Icloud.com>
2021-11-27 01:01:19 -0500

    update deploy methods

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@
 
 module.exports = {
   /* Your site config here */
-  pathPrefix: 'myResumeSite',
+  pathPrefix: '/myResumeSite',
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
